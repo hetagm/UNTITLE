816d0518d4538ce3a498a7ad4396ae77e3d0289c
Khetag <hetagm77@gmail.com>
2022-09-16 16:34:12 +0300

    save

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = num * (-1);
+
+// while (current <= num)
+// {
+//     Console.Write(current);
+//     current = current +1;
+// }
+
+int n = 456; // 450
+int res = n % 10;
+
+int n1 = 456; // 450
+int res = n1 / 10;
