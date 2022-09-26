1c7c3983ab4157c2d02448a1a4affa89dd50e0c6
Khetag <hetagm77@gmail.com>
2022-09-26 18:16:49 +0300

    add task20

---
+++ task20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+// Напишите программу, которая принимает на входе 
+// координаты точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,  6); B(2,  1) -> 5,09
+// A (7, -5); B(1, -1) -> 7,21
+
+Math.Pow(2, 10);
+double num = Math.Sqrt(10);
+//  5,09986564 - 5,09
+double res = Math.Round(num, 2, MidpointRounding.ToZero);
+System.Console.WriteLine(res);
